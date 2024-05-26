f6e8bf45372bf58d240245ede3cdcafdd6b43c10
Anujay Singh <109102210+ImBadAtMaths@users.noreply.github.com>
2024-05-26 10:09:45 +0530

    Added a new feature!
    This function not only displays the value entered but also indicates whether the entered number is greater or smaller than the previous one.

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
-        var message = document.getElementById("message");
-        var attempts = document.getElementById("attempts");
-        var guessInput = document.getElementById("guessInput");
-        var Number = Math.round(Math.random() * 1000 + 1);
-        var count = 0
-        var array = []
-        var element
-        guessButton.addEventListener("click", function () {
-            var userGuess = parseInt(guessInput.value);
-            count++
-            if (array.includes(userGuess)) {
-                message.innerText = "You have already entered this number!"
-                count--
-            } 
-            else if (isNaN(userGuess) || userGuess < 1 || userGuess > 1000) {
-                message.innerText = "Your input is invalid"
-                count--
-            } else if (userGuess > Number) {
-                message.innerText = "Your number is greater than the number generated";
-                array.unshift(userGuess)
-            } else if (userGuess < Number) {
-                message.innerText = "Your number is smaller than the number generated";
-                array.unshift(userGuess)
-            } else if (userGuess == Number) {
-                message.innerText = "congratulation!You got it right";
-                let points = 20 - count - 1;
-                let numberOfGuesses = count + 1;
-                attempts.innerText = `You have scored ${points} out of 20! and Your no of guesses are ${numberOfGuesses}`;
-                return;
-            }
-            if (count >= 20) {
-                attempts.innerText = `You have used all 20 of your attempts`;
-                guessButton.disabled = true;
-            }
+var error = document.getElementById("error");
+var message2 = document.getElementById("message2");
+var attempts = document.getElementById("attempts");
+var guessInput = document.getElementById("guessInput");
+var Number = Math.round(Math.random() * 1000 + 1);
+var count = 0
+var array = []
+guessButton.addEventListener("click", function () {
+    var userGuess = parseInt(guessInput.value);
+    count++
+    if (array.includes(userGuess) && userGuess > Number) {
+        error.innerText = `You have already entered this number!`
+        message2.innerText = `${userGuess} is greater than the number generated`;
+        count--
+    } 
+    else if (array.includes(userGuess) && userGuess < Number) {
+        error.innerText = `You have already entered this number!`
+        message2.innerText = `${userGuess} is smaller than the number generated`;
+        count--
+    } 
+     else if (isNaN(userGuess) || userGuess < 1 || userGuess > 1000) {
+        error.innerText = `Your input is invalid`
+        count--
 
-        })
+    } 
+     else if (userGuess > Number) {
+        message2.innerText = `${userGuess} is greater than the number generated`;
+        array.unshift(userGuess)
+        error.innerText = ""
+
+    } else if (userGuess < Number) {
+        message2.innerText = `${userGuess} is smaller than the number generated`;
+        array.unshift(userGuess)
+        error.innerText = ""
+
+    } else if (userGuess == Number) {
+        error.innerText = ""
+        message2.innerText = `congratulation!You got it right`;
+        let points = 20 - count;
+        let numberOfGuesses = count;
+        attempts.innerText = `You have scored ${points} out of 20! and Your no of guesses are ${numberOfGuesses}`;
+        return;
+    }
+    if (count >= 20) {
+        attempts.innerText = `You have used all 20 of your attempts`;
+        guessButton.disabled = true;
+        error.innerText = ""
+    }
+
+})
